1d1ab0189db2e964a91659e2ec078141badbc2e4
Fernando Acosta <lacosta@gobdigital.com>
2022-06-03 09:55:23 -0500

    Ordering filter para comentarios

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
+from rest_framework.filters import OrderingFilter
 
 
 class CommentsViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    ordering = ['-created_at']
