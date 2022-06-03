bb17fe71ed8072ad09b369f21a9fb0d76f52b1b4
Fernando Acosta <fernandodeveloper99@gmail.com>
2022-06-02 23:02:54 -0500

    Permisos y busqueda por slug

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,9 +1,12 @@
+from cgitb import lookup
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
 
 
 class CategroryModelViewSet(ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
