e675b0710e1cd7703462b49863dfc41cb10f9cef
Fernando Acosta <lacosta@gobdigital.com>
2022-06-03 10:00:59 -0500

    Filtro de comentarios por post

--- comments/api/views.py
+++ comments/api/views.py
@@ -2,9 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentsViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering = ['-created_at']
+    filterset_fields = ['post']
