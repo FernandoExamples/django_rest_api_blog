05002874e1774c3de65ef06821c592a482ca848f
Fernando Acosta <fernandodeveloper99@gmail.com>
2022-06-03 01:07:30 -0500

    Filtro de cvateogrias

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from posts.models import Post
+from users.api.serializers import UserRetrieveSerializer
+from categories.api.serializers import CategorySerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user_data = UserRetrieveSerializer(source='user', read_only=True)
+    category_data = CategorySerializer(source='category', read_only=True)
+
     class Meta:
         model = Post
         fields = ('id', 'title', 'content', 'slug', 'miniature',
-                  'created_at', 'updated_at', 'published', 'user', 'category')
+                  'created_at', 'updated_at', 'published', 'user', 'category', 'user_data', 'category_data')
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 from posts.api.permissions import IsAdminOrReadOnly
@@ -8,3 +9,6 @@ class PostApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'category__slug']
+    # filterset_fields = ['category']
