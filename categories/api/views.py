66ffd7a5ce74f9854374a9a5aa969a3ddac177cd
Fernando Acosta <fernandodeveloper99@gmail.com>
2022-06-02 23:55:17 -0500

    Filtro para categorias

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,5 +1,7 @@
 from cgitb import lookup
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
 from categories.api.permissions import IsAdminOrReadOnly
@@ -7,6 +9,9 @@ from categories.api.permissions import IsAdminOrReadOnly
 
 class CategroryModelViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'slug'
+    queryset = Category.objects.all()
+    # queryset = Category.objects.filter(published=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['published']
